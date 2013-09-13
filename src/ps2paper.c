255fa966f90a4cc2a477b048e6d6d1cbeb3ffad1
Steve Fryatt <info@stevefryatt.org.uk>
2013-09-13 23:50:35 +0000

    Add PS2 Paper dialogue and implement basic paper info display.

--- src/ps2paper.c
+++ src/ps2paper.c
@@ -35,12 +35,15 @@
 
 /* OSLib header files */
 
+#include "oslib/fileswitch.h"
+#include "oslib/osfile.h"
 #include "oslib/wimp.h"
 
 /* SF-Lib header files. */
 
 #include "sflib/config.h"
 #include "sflib/debug.h"
+#include "sflib/errors.h"
 #include "sflib/event.h"
 #include "sflib/icons.h"
 #include "sflib/menus.h"
@@ -56,11 +59,21 @@
 #include "templates.h"
 
 #define PS2PAPER_NAME_LEN 128
+#define PS2PAPER_SOURCE_LEN 32
+#define PS2PAPER_ICON_HEIGHT 48
 
 /* Paper Window icons. */
 
-//#define PAPER_ICON_CANCEL 0
-//#define PAPER_ICON_OK 1
+#define PS2PAPER_ICON_PANE 0
+
+#define PS2PAPER_PANE_NAME 0
+#define PS2PAPER_PANE_X 1
+#define PS2PAPER_PANE_Y 2
+#define PS2PAPER_PANE_SOURCE 3
+#define PS2PAPER_PANE_FILE 4
+
+#define PS2PAPER_ICON_CLOSE 1
+#define PS2PAPER_ICON_UPDATE 2
 //#define PAPER_ICON_SIZE_X 3
 //#define PAPER_ICON_SIZE_Y 4
 //#define PAPER_ICON_MM 5
@@ -69,17 +82,30 @@
 
 
 struct ps2paper_size {
-	char			name[PS2PAPER_NAME_LEN];/**< The Printers name for the paper		*/
-	int			width;			/**< The Printers width of the paper		*/
-	int			height;			/**< The Printers height of the paper		*/
+	char			name[PS2PAPER_NAME_LEN];	/**< The Printers name for the paper		*/
+	int			width;				/**< The Printers width of the paper		*/
+	int			height;				/**< The Printers height of the paper		*/
+	char			source[PS2PAPER_SOURCE_LEN];	/**< The name of the source file		*/
+	char			ps2_file[PS2PAPER_NAME_LEN];	/**< The associated PS2 Paper file, or ""	*/
+	osbool			ps2_file_exists;		/**< TRUE if the PS2 file exists; else FALSE	*/
 	
-	struct ps2paper_size	*next;			/**< Link to the next paper size.		*/
+	struct ps2paper_size	*next;				/**< Link to the next paper size.		*/
 };
 
-static struct ps2paper_size	*paper_sizes = NULL;	/**< Linked list of paper sizes.		*/
-static unsigned			paper_size_count;	/**< Number of defined paper sizes.		*/
+static struct ps2paper_size	*paper_sizes = NULL;		/**< Linked list of paper sizes.		*/
+static unsigned			paper_count = 0;		/**< Number of defined paper sizes.		*/
+static struct ps2paper_size	**redraw_list = NULL;		/**< Redraw list for paper sizes.		*/
+	
+static wimp_w			ps2paper_window = NULL;		/**< The paper list window.			*/
+static wimp_w			ps2paper_pane = NULL;		/**< The paper list pane.			*/
+static wimp_window		*ps2paper_pane_def = NULL;	/**< The paper list pane defintion.		*/
 
-static osbool ps2paper_read_def_file(char *file);
+static void	ps2paper_close_window(void);
+static void	ps2paper_pane_redraw_handler(wimp_draw *redraw);
+static void	ps2paper_click_handler(wimp_pointer *pointer);
+static void	ps2paper_clear_definitions(void);
+static void	ps2paper_read_definitions(void);
+static osbool	ps2paper_read_def_file(char *file, char *source);
 
 /**
  * Initialise the ps2paper dialogue.
@@ -87,10 +113,20 @@ static osbool ps2paper_read_def_file(char *file);
 
 void ps2paper_initialise(void)
 {
-	//paper_window = templates_create_window("Paper");
-	//ihelp_add_window(paper_window, "Paper", NULL);
+	ps2paper_window = templates_create_window("PsPaper");
+	ihelp_add_window(ps2paper_window, "PsPaper", NULL);
 
-	//event_add_window_mouse_event(paper_window, paper_click_handler);
+	ps2paper_pane_def = templates_load_window("PsPaperPane");
+	ps2paper_pane_def->icon_count = 0;
+	ps2paper_pane = wimp_create_window(ps2paper_pane_def);
+	ihelp_add_window(ps2paper_pane, "PsPaperPane", NULL /*convert_decode_queue_pane_help*/);
+
+	event_add_window_redraw_event(ps2paper_pane, ps2paper_pane_redraw_handler);
+	//event_add_window_mouse_event(ps2paper_pane convert_queue_pane_click_handler);
+
+	paper_count = 5;
+
+	event_add_window_mouse_event(ps2paper_window, ps2paper_click_handler);
 	//event_add_window_key_event(paper_window, paper_keypress_handler);
 	
 	//event_add_window_icon_radio(paper_window, PAPER_ICON_MM, TRUE);
@@ -100,46 +136,215 @@ void ps2paper_initialise(void)
 
 
 /**
- * Open the paper editing window on screen.
+ * Open the PS2 Paper dialogue.
  *
  * \param *pointer	The pointer location at which to open the window.
  */
 
 void ps2paper_open_window(wimp_pointer *pointer)
 {
-	struct ps2paper_size	*paper;
-
-	debug_printf("\\YPrinters:PaperRO");
-	ps2paper_read_def_file("Printers:PaperRO");
-	debug_printf("\\YPrinterChoices:PaperRW");
-	ps2paper_read_def_file("PrinterChoices:PaperRW");
-	debug_printf("\\YPrinters:ps.Resources.PaperRO");
-	ps2paper_read_def_file("Printers:ps.Resources.PaperRO");
-	
-	paper = paper_sizes;
-	
-	while (paper != NULL) {
-		debug_printf("Paper '%s', width %u, height %u", paper->name, paper->width, paper->height);
-		
-		paper = paper->next;
-	}
-
+	windows_open_with_pane_centred_at_pointer(ps2paper_window, ps2paper_pane, PS2PAPER_ICON_PANE, 40, pointer);
+	ps2paper_read_definitions();
 }
 
+
+/**
+ * Close the PS2 Paper dialogue.
+ */
+
+static void ps2paper_close_window(void)
+{
+	wimp_close_window(ps2paper_window);
+	ps2paper_clear_definitions();
+}
+
+
+/**
+ * Process redraw requests for the PS2 Paper dialogue pane.
+ *
+ * \param *redraw		The redraw event block to handle.
+ */
+
+static void ps2paper_pane_redraw_handler(wimp_draw *redraw)
+{
+	int			ox, oy, top, base, y;
+	osbool			more;
+	wimp_icon		*icon;
+	char			buffer[128];
+
+	/* Perform the redraw if a window was found. */
+
+	if (redraw->w != ps2paper_pane)
+		return;
+
+	more = wimp_redraw_window(redraw);
+
+	ox = redraw->box.x0 - redraw->xscroll;
+	oy = redraw->box.y1 - redraw->yscroll;
+
+	icon = ps2paper_pane_def->icons;
+
+	while (more) {
+		top = (oy - redraw->clip.y1) / PS2PAPER_ICON_HEIGHT;
+		if (top < 0)
+			top = 0;
+
+		base = (PS2PAPER_ICON_HEIGHT + (PS2PAPER_ICON_HEIGHT / 2) + oy - redraw->clip.y0) / PS2PAPER_ICON_HEIGHT;
+
+		for (y = top; y < paper_count && y <= base; y++) {
+			icon[PS2PAPER_PANE_NAME].extent.y1 = -(y * PS2PAPER_ICON_HEIGHT);
+			icon[PS2PAPER_PANE_NAME].extent.y0 = icon[PS2PAPER_PANE_NAME].extent.y1 - PS2PAPER_ICON_HEIGHT;
+			icon[PS2PAPER_PANE_NAME].data.indirected_text_and_sprite.text = (redraw_list[y])->name;
+			icon[PS2PAPER_PANE_NAME].data.indirected_text_and_sprite.size = PS2PAPER_NAME_LEN;
+
+			wimp_plot_icon(&(icon[PS2PAPER_PANE_NAME]));
+
+			snprintf(buffer, sizeof(buffer), "%.3f", (double) (redraw_list[y])->width / 72000.0);
+
+			icon[PS2PAPER_PANE_X].extent.y1 = -(y * PS2PAPER_ICON_HEIGHT);
+			icon[PS2PAPER_PANE_X].extent.y0 = icon[PS2PAPER_PANE_X].extent.y1 - PS2PAPER_ICON_HEIGHT;
+			icon[PS2PAPER_PANE_X].data.indirected_text_and_sprite.text = buffer;
+			icon[PS2PAPER_PANE_X].data.indirected_text_and_sprite.size = sizeof(buffer);
+
+			wimp_plot_icon(&(icon[PS2PAPER_PANE_X]));
+
+			snprintf(buffer, sizeof(buffer), "%.3f", (double) (redraw_list[y])->height / 72000.0);
+
+			icon[PS2PAPER_PANE_Y].extent.y1 = -(y * PS2PAPER_ICON_HEIGHT);
+			icon[PS2PAPER_PANE_Y].extent.y0 = icon[PS2PAPER_PANE_Y].extent.y1 - PS2PAPER_ICON_HEIGHT;
+			icon[PS2PAPER_PANE_Y].data.indirected_text_and_sprite.text = buffer;
+			icon[PS2PAPER_PANE_Y].data.indirected_text_and_sprite.size = sizeof(buffer);
+
+			wimp_plot_icon(&(icon[PS2PAPER_PANE_Y]));
+			
+			icon[PS2PAPER_PANE_SOURCE].extent.y1 = -(y * PS2PAPER_ICON_HEIGHT);
+			icon[PS2PAPER_PANE_SOURCE].extent.y0 = icon[PS2PAPER_PANE_SOURCE].extent.y1 - PS2PAPER_ICON_HEIGHT;
+			icon[PS2PAPER_PANE_SOURCE].data.indirected_text_and_sprite.text = (redraw_list[y])->source;
+			icon[PS2PAPER_PANE_SOURCE].data.indirected_text_and_sprite.size = PS2PAPER_SOURCE_LEN;
+
+			wimp_plot_icon(&(icon[PS2PAPER_PANE_SOURCE]));
+			
+			icon[PS2PAPER_PANE_FILE].extent.y1 = -(y * PS2PAPER_ICON_HEIGHT);
+			icon[PS2PAPER_PANE_FILE].extent.y0 = icon[PS2PAPER_PANE_FILE].extent.y1 - PS2PAPER_ICON_HEIGHT;
+			icon[PS2PAPER_PANE_FILE].data.indirected_text_and_sprite.text = (redraw_list[y])->ps2_file;
+			icon[PS2PAPER_PANE_FILE].data.indirected_text_and_sprite.size = PS2PAPER_NAME_LEN;
+
+			if ((redraw_list[y])->ps2_file_exists)
+				wimp_plot_icon(&(icon[PS2PAPER_PANE_FILE]));
+
+
+			/*icon[QUEUE_PANE_INCLUDE].extent.y1 = -(y * QUEUE_ICON_HEIGHT);
+			icon[QUEUE_PANE_INCLUDE].extent.y0 = icon[QUEUE_PANE_INCLUDE].extent.y1 - QUEUE_ICON_HEIGHT;
+			icon[QUEUE_PANE_INCLUDE].data.indirected_sprite.id =
+					(osspriteop_id) (((queue_redraw_list[y])->include) ? "opton" : "optoff");
+			icon[QUEUE_PANE_INCLUDE].data.indirected_sprite.area = (osspriteop_area *) 1;
+			icon[QUEUE_PANE_INCLUDE].data.indirected_sprite.size = 12;
+
+			wimp_plot_icon(&(icon[QUEUE_PANE_INCLUDE])); */
+			
+
+			/*icon[QUEUE_PANE_DELETE].extent.y1 = -(y * QUEUE_ICON_HEIGHT);
+			icon[QUEUE_PANE_DELETE].extent.y0 = icon[QUEUE_PANE_DELETE].extent.y1 - QUEUE_ICON_HEIGHT;
+			icon[QUEUE_PANE_DELETE].data.indirected_sprite.id =
+					(osspriteop_id) (((queue_redraw_list[y])->object_type == DELETED) ? "del1" : "del0");
+			icon[QUEUE_PANE_DELETE].data.indirected_sprite.area = main_wimp_sprites;
+			icon[QUEUE_PANE_DELETE].data.indirected_sprite.size = 12;
+
+			wimp_plot_icon(&(icon[QUEUE_PANE_DELETE])); */
+		}
+
+		more = wimp_get_rectangle (redraw);
+	}
+}
+
+
+/**
+ * Process mouse clicks in the PS2 Paper dialogue.
+ *
+ * \param *pointer		The mouse event block to handle.
+ */
+
+static void ps2paper_click_handler(wimp_pointer *pointer)
+{
+	if (pointer == NULL)
+		return;
+
+	switch ((int) pointer->i) {
+	case PS2PAPER_ICON_CLOSE:
+		ps2paper_close_window();
+		break;
+
+	case PS2PAPER_ICON_UPDATE:
+		ps2paper_close_window();
+		//convert_start_held_conversion();
+		break;
+	}
+}
+
+
+static void ps2paper_read_definitions(void)
+{
+	struct ps2paper_size	*paper;
+	int			i;
+	char			source[PS2PAPER_SOURCE_LEN];
+
+	ps2paper_clear_definitions();
+
+	msgs_lookup("PaperFileM", source, PS2PAPER_SOURCE_LEN);
+	ps2paper_read_def_file("Printers:PaperRO", source);
+	msgs_lookup("PaperFileU", source, PS2PAPER_SOURCE_LEN);
+	ps2paper_read_def_file("PrinterChoices:PaperRW", source);
+	msgs_lookup("PaperFileD", source, PS2PAPER_SOURCE_LEN);
+	ps2paper_read_def_file("Printers:ps.Resources.PaperRO", source);
+	
+	redraw_list = malloc(paper_count * sizeof(struct ps2paper_size *));
+	if (redraw_list == NULL) {
+		error_msgs_report_error("PaperNoMem");
+		ps2paper_clear_definitions();
+		return;
+	}
+	
+	paper = paper_sizes;
+	i = paper_count;
+	
+	while (i > 0 && paper != NULL) {
+		redraw_list[--i] = paper;
+		paper = paper->next;
+	}
+}
+
+static void ps2paper_clear_definitions(void)
+{
+	struct ps2paper_size	*paper, *current;
+	
+	paper = paper_sizes;
+	paper_sizes = NULL;
+	paper_count = 0;
+	
+	while (paper != NULL) {
+		current = paper;
+		paper = paper->next;
+		free(current);
+	}
+}
 
 /**
  * Process the contents of a Printers paper file, reading the paper definitions
  * and adding them to the list of sizes.
  *
  * \param *file		The name of the file to be read in.
+ * \param *source	The "user facing" name for the file.
  */
 
-static osbool ps2paper_read_def_file(char *file)
+static osbool ps2paper_read_def_file(char *file, char *source)
 {
 	FILE			*in;
 	char			line[1024], *clean, *data, paper_name[PS2PAPER_NAME_LEN];
+	int			i;
 	unsigned		paper_width, paper_height;
 	struct ps2paper_size	*paper_definition;
+	os_error		*error;
+	fileswitch_object_type	type;
 
 
 	if (file == NULL || *file == '\0')
@@ -177,11 +382,28 @@ static osbool ps2paper_read_def_file(char *file)
 			
 			if (paper_definition != NULL) {
 				strncpy(paper_definition->name, paper_name, PS2PAPER_NAME_LEN);
+				strncpy(paper_definition->source, source, PS2PAPER_SOURCE_LEN);
 				paper_definition->width = paper_width;
 				paper_definition->height = paper_height;
 				
+				for (i = 0; i < PS2PAPER_NAME_LEN && paper_name[i] != '\0' && paper_name[i] != ' '; i++)
+					paper_definition->ps2_file[i] = paper_name[i];
+				
+				paper_definition->ps2_file[i] = '\0';
+				paper_definition->ps2_file_exists = FALSE;
+				
+				if (paper_definition->ps2_file[0] != '\0') {
+					snprintf(line, sizeof(line), "Printers:ps.Paper.%s", paper_definition->ps2_file);
+					error = xosfile_read_no_path(line, &type, NULL, NULL, NULL, NULL);
+					
+					if (error == NULL && type == fileswitch_IS_FILE)
+						paper_definition->ps2_file_exists = TRUE;
+				}
+				
 				paper_definition->next = paper_sizes;
 				paper_sizes = paper_definition;
+				
+				paper_count++;
 			}
 
 			*paper_name = '\0';
