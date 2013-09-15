ada98e05df6e99dfc2dfee835ab692e498eb42ec
Steve Fryatt <info@stevefryatt.org.uk>
2013-09-15 20:35:33 +0000

    Implement unit display and selection is PS2 paper list.

--- src/ps2paper.c
+++ src/ps2paper.c
@@ -71,15 +71,28 @@
 #define PS2PAPER_PANE_Y 2
 #define PS2PAPER_PANE_SOURCE 3
 #define PS2PAPER_PANE_FILE 4
+#define PS2PAPER_PANE_STATUS 5
 
 #define PS2PAPER_ICON_CLOSE 1
 #define PS2PAPER_ICON_UPDATE 2
+#define PS2PAPER_ICON_INCH 3
+#define PS2PAPER_ICON_MM 4
+#define PS2PAPER_ICON_POINT 5
 //#define PAPER_ICON_SIZE_X 3
 //#define PAPER_ICON_SIZE_Y 4
-//#define PAPER_ICON_MM 5
-//#define PAPER_ICON_INCH 6
-//#define PAPER_ICON_POINT 7
 
+enum ps2paper_status {
+	PS2PAPER_STATUS_MISSING,				/**< There is no file for the paper size.	*/
+	PS2PAPER_STATUS_UNKNOWN,				/**< There is a file, but it's not one of ours.	*/
+	PS2PAPER_STATUS_CORRECT,				/**< There is a file, and it matches the paper.	*/
+	PS2PAPER_STATUS_INCORRECT				/**< There is a file, but the size is wrong.	*/
+};
+
+enum ps2paper_units {
+	PS2PAPER_UNITS_MM = 0,
+	PS2PAPER_UNITS_INCH = 1,
+	PS2PAPER_UNITS_POINT = 2
+};
 
 struct ps2paper_size {
 	char			name[PS2PAPER_NAME_LEN];	/**< The Printers name for the paper		*/
@@ -87,18 +100,20 @@ struct ps2paper_size {
 	int			height;				/**< The Printers height of the paper		*/
 	char			source[PS2PAPER_SOURCE_LEN];	/**< The name of the source file		*/
 	char			ps2_file[PS2PAPER_NAME_LEN];	/**< The associated PS2 Paper file, or ""	*/
-	osbool			ps2_file_exists;		/**< TRUE if the PS2 file exists; else FALSE	*/
-	
+	enum ps2paper_status	ps2_file_status;		/**< Indicate the status of the Paper File.	*/
+
 	struct ps2paper_size	*next;				/**< Link to the next paper size.		*/
 };
 
 static struct ps2paper_size	*paper_sizes = NULL;		/**< Linked list of paper sizes.		*/
 static unsigned			paper_count = 0;		/**< Number of defined paper sizes.		*/
 static struct ps2paper_size	**redraw_list = NULL;		/**< Redraw list for paper sizes.		*/
-	
+
 static wimp_w			ps2paper_window = NULL;		/**< The paper list window.			*/
 static wimp_w			ps2paper_pane = NULL;		/**< The paper list pane.			*/
 static wimp_window		*ps2paper_pane_def = NULL;	/**< The paper list pane defintion.		*/
+
+static enum ps2paper_units	page_display_unit = PS2PAPER_UNITS_MM;
 
 static void	ps2paper_close_window(void);
 static void	ps2paper_pane_redraw_handler(wimp_draw *redraw);
@@ -128,10 +143,10 @@ void ps2paper_initialise(void)
 
 	event_add_window_mouse_event(ps2paper_window, ps2paper_click_handler);
 	//event_add_window_key_event(paper_window, paper_keypress_handler);
-	
-	//event_add_window_icon_radio(paper_window, PAPER_ICON_MM, TRUE);
-	//event_add_window_icon_radio(paper_window, PAPER_ICON_INCH, TRUE);
-	//event_add_window_icon_radio(paper_window, PAPER_ICON_POINT, TRUE);
+
+	event_add_window_icon_radio(ps2paper_window, PS2PAPER_ICON_MM, FALSE);
+	event_add_window_icon_radio(ps2paper_window, PS2PAPER_ICON_INCH, FALSE);
+	event_add_window_icon_radio(ps2paper_window, PS2PAPER_ICON_POINT, FALSE);
 }
 
 
@@ -143,6 +158,10 @@ void ps2paper_initialise(void)
 
 void ps2paper_open_window(wimp_pointer *pointer)
 {
+	icons_set_selected(ps2paper_window, PS2PAPER_ICON_MM, (page_display_unit == PS2PAPER_UNITS_MM) ? TRUE : FALSE);
+	icons_set_selected(ps2paper_window, PS2PAPER_ICON_INCH, (page_display_unit == PS2PAPER_UNITS_INCH) ? TRUE : FALSE);
+	icons_set_selected(ps2paper_window, PS2PAPER_ICON_POINT, (page_display_unit == PS2PAPER_UNITS_POINT) ? TRUE : FALSE);
+
 	windows_open_with_pane_centred_at_pointer(ps2paper_window, ps2paper_pane, PS2PAPER_ICON_PANE, 40, pointer);
 	ps2paper_read_definitions();
 }
@@ -170,7 +189,8 @@ static void ps2paper_pane_redraw_handler(wimp_draw *redraw)
 	int			ox, oy, top, base, y;
 	osbool			more;
 	wimp_icon		*icon;
-	char			buffer[128];
+	char			buffer[128], *status, *unit_format;
+	double			unit_scale;
 
 	/* Perform the redraw if a window was found. */
 
@@ -183,6 +203,22 @@ static void ps2paper_pane_redraw_handler(wimp_draw *redraw)
 	oy = redraw->box.y1 - redraw->yscroll;
 
 	icon = ps2paper_pane_def->icons;
+
+	switch (page_display_unit) {
+	case PS2PAPER_UNITS_MM:
+		unit_scale = 2836.42567;
+		unit_format = "%.1f";
+		break;
+	case PS2PAPER_UNITS_INCH:
+		unit_scale = 72000.0;
+		unit_format = "%.3f";
+		break;
+	case PS2PAPER_UNITS_POINT:
+	default:
+		unit_scale = 1000.0;
+		unit_format = "%.1f";
+		break;
+	}
 
 	while (more) {
 		top = (oy - redraw->clip.y1) / PS2PAPER_ICON_HEIGHT;
@@ -199,7 +235,7 @@ static void ps2paper_pane_redraw_handler(wimp_draw *redraw)
 
 			wimp_plot_icon(&(icon[PS2PAPER_PANE_NAME]));
 
-			snprintf(buffer, sizeof(buffer), "%.3f", (double) (redraw_list[y])->width / 72000.0);
+			snprintf(buffer, sizeof(buffer), unit_format, (double) (redraw_list[y])->width / unit_scale);
 
 			icon[PS2PAPER_PANE_X].extent.y1 = -(y * PS2PAPER_ICON_HEIGHT);
 			icon[PS2PAPER_PANE_X].extent.y0 = icon[PS2PAPER_PANE_X].extent.y1 - PS2PAPER_ICON_HEIGHT;
@@ -208,7 +244,7 @@ static void ps2paper_pane_redraw_handler(wimp_draw *redraw)
 
 			wimp_plot_icon(&(icon[PS2PAPER_PANE_X]));
 
-			snprintf(buffer, sizeof(buffer), "%.3f", (double) (redraw_list[y])->height / 72000.0);
+			snprintf(buffer, sizeof(buffer), unit_format, (double) (redraw_list[y])->height / unit_scale);
 
 			icon[PS2PAPER_PANE_Y].extent.y1 = -(y * PS2PAPER_ICON_HEIGHT);
 			icon[PS2PAPER_PANE_Y].extent.y0 = icon[PS2PAPER_PANE_Y].extent.y1 - PS2PAPER_ICON_HEIGHT;
@@ -216,21 +252,48 @@ static void ps2paper_pane_redraw_handler(wimp_draw *redraw)
 			icon[PS2PAPER_PANE_Y].data.indirected_text_and_sprite.size = sizeof(buffer);
 
 			wimp_plot_icon(&(icon[PS2PAPER_PANE_Y]));
-			
+
 			icon[PS2PAPER_PANE_SOURCE].extent.y1 = -(y * PS2PAPER_ICON_HEIGHT);
 			icon[PS2PAPER_PANE_SOURCE].extent.y0 = icon[PS2PAPER_PANE_SOURCE].extent.y1 - PS2PAPER_ICON_HEIGHT;
 			icon[PS2PAPER_PANE_SOURCE].data.indirected_text_and_sprite.text = (redraw_list[y])->source;
 			icon[PS2PAPER_PANE_SOURCE].data.indirected_text_and_sprite.size = PS2PAPER_SOURCE_LEN;
 
 			wimp_plot_icon(&(icon[PS2PAPER_PANE_SOURCE]));
-			
+
 			icon[PS2PAPER_PANE_FILE].extent.y1 = -(y * PS2PAPER_ICON_HEIGHT);
 			icon[PS2PAPER_PANE_FILE].extent.y0 = icon[PS2PAPER_PANE_FILE].extent.y1 - PS2PAPER_ICON_HEIGHT;
 			icon[PS2PAPER_PANE_FILE].data.indirected_text_and_sprite.text = (redraw_list[y])->ps2_file;
 			icon[PS2PAPER_PANE_FILE].data.indirected_text_and_sprite.size = PS2PAPER_NAME_LEN;
 
-			if ((redraw_list[y])->ps2_file_exists)
+			if ((redraw_list[y])->ps2_file_status != PS2PAPER_STATUS_MISSING)
 				wimp_plot_icon(&(icon[PS2PAPER_PANE_FILE]));
+
+			switch((redraw_list[y])->ps2_file_status) {
+			case PS2PAPER_STATUS_MISSING:
+				status = "PaperStatMiss";
+				break;
+			case PS2PAPER_STATUS_UNKNOWN:
+				status = "PaperStatUnkn";
+				break;
+			case PS2PAPER_STATUS_CORRECT:
+				status = "PaperStatOK";
+				break;
+			case PS2PAPER_STATUS_INCORRECT:
+				status = "PaperStatNOK";
+				break;
+			default:
+				status = "";
+				break;
+			}
+
+			msgs_lookup(status, buffer, sizeof(buffer));
+
+			icon[PS2PAPER_PANE_STATUS].extent.y1 = -(y * PS2PAPER_ICON_HEIGHT);
+			icon[PS2PAPER_PANE_STATUS].extent.y0 = icon[PS2PAPER_PANE_STATUS].extent.y1 - PS2PAPER_ICON_HEIGHT;
+			icon[PS2PAPER_PANE_STATUS].data.indirected_text_and_sprite.text = buffer;
+			icon[PS2PAPER_PANE_STATUS].data.indirected_text_and_sprite.size = sizeof(buffer);
+
+			wimp_plot_icon(&(icon[PS2PAPER_PANE_STATUS]));
 
 
 			/*icon[QUEUE_PANE_INCLUDE].extent.y1 = -(y * QUEUE_ICON_HEIGHT);
@@ -241,7 +304,7 @@ static void ps2paper_pane_redraw_handler(wimp_draw *redraw)
 			icon[QUEUE_PANE_INCLUDE].data.indirected_sprite.size = 12;
 
 			wimp_plot_icon(&(icon[QUEUE_PANE_INCLUDE])); */
-			
+
 
 			/*icon[QUEUE_PANE_DELETE].extent.y1 = -(y * QUEUE_ICON_HEIGHT);
 			icon[QUEUE_PANE_DELETE].extent.y0 = icon[QUEUE_PANE_DELETE].extent.y1 - QUEUE_ICON_HEIGHT;
@@ -270,6 +333,21 @@ static void ps2paper_click_handler(wimp_pointer *pointer)
 		return;
 
 	switch ((int) pointer->i) {
+	case PS2PAPER_ICON_MM:
+		page_display_unit = PS2PAPER_UNITS_MM;
+		windows_redraw(ps2paper_pane);
+		break;
+
+	case PS2PAPER_ICON_INCH:
+		page_display_unit = PS2PAPER_UNITS_INCH;
+		windows_redraw(ps2paper_pane);
+		break;
+
+	case PS2PAPER_ICON_POINT:
+		page_display_unit = PS2PAPER_UNITS_POINT;
+		windows_redraw(ps2paper_pane);
+		break;
+
 	case PS2PAPER_ICON_CLOSE:
 		ps2paper_close_window();
 		break;
@@ -296,17 +374,17 @@ static void ps2paper_read_definitions(void)
 	ps2paper_read_def_file("PrinterChoices:PaperRW", source);
 	msgs_lookup("PaperFileD", source, PS2PAPER_SOURCE_LEN);
 	ps2paper_read_def_file("Printers:ps.Resources.PaperRO", source);
-	
+
 	redraw_list = malloc(paper_count * sizeof(struct ps2paper_size *));
 	if (redraw_list == NULL) {
 		error_msgs_report_error("PaperNoMem");
 		ps2paper_clear_definitions();
 		return;
 	}
-	
+
 	paper = paper_sizes;
 	i = paper_count;
-	
+
 	while (i > 0 && paper != NULL) {
 		redraw_list[--i] = paper;
 		paper = paper->next;
@@ -316,11 +394,11 @@ static void ps2paper_read_definitions(void)
 static void ps2paper_clear_definitions(void)
 {
 	struct ps2paper_size	*paper, *current;
-	
+
 	paper = paper_sizes;
 	paper_sizes = NULL;
 	paper_count = 0;
-	
+
 	while (paper != NULL) {
 		current = paper;
 		paper = paper->next;
@@ -354,7 +432,7 @@ static osbool ps2paper_read_def_file(char *file, char *source)
 
 	if (in == NULL)
 		return FALSE;
-	
+
 	*paper_name = '\0';
 	paper_width = 0;
 	paper_height = 0;
@@ -362,10 +440,10 @@ static osbool ps2paper_read_def_file(char *file, char *source)
 	while (fgets(line, sizeof(line), in) != NULL) {
 		string_ctrl_zero_terminate(line);
 		clean = string_strip_surrounding_whitespace(line);
-	
+
 		if (*clean == '\0' || *clean == '#')
 			continue;
-		
+
 		if (strstr(clean, "pn:") == clean) {
 			data = string_strip_surrounding_whitespace(clean + 3);
 			strncpy(paper_name, data, PS2PAPER_NAME_LEN);
@@ -376,33 +454,33 @@ static osbool ps2paper_read_def_file(char *file, char *source)
 			data = string_strip_surrounding_whitespace(clean + 3);
 			paper_height = atoi(data);
 		}
-		
+
 		if (paper_name != '\0' && paper_width != 0 && paper_height != 0) {
 			paper_definition = malloc(sizeof(struct ps2paper_size));
-			
+
 			if (paper_definition != NULL) {
 				strncpy(paper_definition->name, paper_name, PS2PAPER_NAME_LEN);
 				strncpy(paper_definition->source, source, PS2PAPER_SOURCE_LEN);
 				paper_definition->width = paper_width;
 				paper_definition->height = paper_height;
-				
+
 				for (i = 0; i < PS2PAPER_NAME_LEN && paper_name[i] != '\0' && paper_name[i] != ' '; i++)
 					paper_definition->ps2_file[i] = paper_name[i];
-				
+
 				paper_definition->ps2_file[i] = '\0';
-				paper_definition->ps2_file_exists = FALSE;
-				
+				paper_definition->ps2_file_status = PS2PAPER_STATUS_MISSING;
+
 				if (paper_definition->ps2_file[0] != '\0') {
 					snprintf(line, sizeof(line), "Printers:ps.Paper.%s", paper_definition->ps2_file);
 					error = xosfile_read_no_path(line, &type, NULL, NULL, NULL, NULL);
-					
+
 					if (error == NULL && type == fileswitch_IS_FILE)
-						paper_definition->ps2_file_exists = TRUE;
+						paper_definition->ps2_file_status = PS2PAPER_STATUS_UNKNOWN;
 				}
-				
+
 				paper_definition->next = paper_sizes;
 				paper_sizes = paper_definition;
-				
+
 				paper_count++;
 			}
 
