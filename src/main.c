7b4944d2582e935be0f60479f60e060e70409c4b
Steve Fryatt <info@stevefryatt.org.uk>
2016-10-21 18:18:13 +0000

    Trap missing application sprites files with a useful error.

--- src/main.c
+++ src/main.c
@@ -276,6 +276,8 @@ static void main_initialise(void)
 	/* Load the window templates. */
 
 	sprites = resources_load_user_sprite_area("<PrintPDF$Dir>.Sprites");
+	if (sprites == NULL)
+		error_msgs_report_fatal("NoSprites");
 
 	main_wimp_sprites = sprites;
 
